e597cb8300202a26a2f131885f3eea7059dc63cd
TomasJefferson2J <1303874@senati.pe>
2023-03-11 19:13:51 -0500

    apkactualizacion

--- app/src/main/java/aplicacion/app/apk2/MainActivity.java
+++ app/src/main/java/aplicacion/app/apk2/MainActivity.java
@@ -3,6 +3,7 @@ package aplicacion.app.apk2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,14 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         et1 = findViewById(R.id.et1);
         et2 = findViewById(R.id.et2);
     }
 
-    public void Mayor(View view) {
-        string n1 = et1.getText().toString();
-        string n2 = et2.getText().toString();
+    public void MAYOR(View view) {
+        String n1 = et1.getText().toString();
+        String n2 = et2.getText().toString();
 
         int k1 = Integer.parseInt(n1);
         int k2 = Integer.parseInt(n2);
@@ -35,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void Menor(View view){
-        string n1 = et1.getText().toString();
-        string n2 = et2.getText().toString();
+    public void MENOR(View view) {
+        String n1 = et1.getText().toString();
+        String n2 = et2.getText().toString();
 
         int k1 = Integer.parseInt(n1);
         int k2 = Integer.parseInt(n2);
